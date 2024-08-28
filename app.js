019ab55417f9cca15cdb950dd8b339ee01d145d1
NouraKuthub <noura.kuthub1234@gmail.com>
2024-08-28 12:24:26 +0530

    NodeMailer backend

---
+++ app.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const nodemailer = require("nodemailer");
+const cors = require("cors");
+const app = express();
+const port = 5000;
+
+app.use(cors());
+app.use(express.json({ limit: "25mb" }));
+app.use(express.urlencoded({ limit: "25mb" }));
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  next();
+});
+
+function sendEmail({ name, phone, email, subject, message }) {
+  return new Promise((resolve, reject) => {
+    var transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: "noura.devdesign@gmail.com",
+        pass: "mtfskaqjyjzkikam",
+      },
+    });
+
+    const mail_configs = {
+      from: 'noura.kuthub1234@gmail.com',
+      to: 'noura.devdesign@gmail.com',
+      subject: subject,
+      text: `
+      Name: ${name}\nPhone: ${phone}\nEmail: ${email}\nMessage: ${message}
+      Best Regards
+      `,
+    };
+    transporter.sendMail(mail_configs, function (error, info) {
+      if (error) {
+        console.log(error);
+        return reject({ message: `An error has occurred` });
+      }
+      return resolve({ message: "Email sent successfully" });
+    });
+  }); 
+}
+
+app.get("/", (req, res) => {
+  sendEmail(req.query)
+    .then((response) => res.send(response.message))
+    .catch((error) => res.status(500).send(error.message));
+});
+
+app.listen(port, () => {
+  console.log(`nodemailerProject is listening at http://localhost:${port}`);
+});
